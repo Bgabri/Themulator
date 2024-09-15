5316ade9461987e0bd7232c7ceda6778470f27cc
Bgabri <76167293+Bgabri@users.noreply.github.com>
2024-09-15 15:22:30 +0200

    basic exit errors

--- src/Test/main.c
+++ src/Test/main.c
@@ -2,20 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
-#include "ensure.h"
-
-INCREMENT_COUNT(0, 4, 2);
 
 int main(int argc, char const* argv[]) {
 
     int n ;
     scanf("%d\n", &n);
-    if (n == -1) return 1;
+    // if (n == -1) return 1;
+    int wa[1];
 
     for (int i = 0; i < n; i++) {
         int a, b;
         scanf("%d %d", &a, &b);
-        printf("%d\n", a+b);
+        printf("%d\n", wa[n]);
     }
     return 0;
 }
