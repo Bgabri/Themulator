68e25a873f30ffab11c1f23a86d5a7d5c8f109cf
Bgabri <76167293+Bgabri@users.noreply.github.com>
2024-11-13 20:24:56 +0100

    update tests

--- Tests/test.c
+++ Tests/test.c
@@ -5,7 +5,10 @@ int main() {
     char buffer[1024];  // Buffer to store each line of input
 
     while (fgets(buffer, sizeof(buffer), stdin)) {
-        system(buffer);
+        int result = system(buffer);
+        if (result != 0) {
+            exit(result);
+        };
     }
 
     return 0;
